8dfa95686726879aa117f894d76d4e6605cce8f7
tobi <Tobias20.3@hotmail.de>
2023-04-18 13:26:11 +0200

    Add random initialization method and input handling

--- cellular_automaton/main.py
+++ cellular_automaton/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+NUM_ITERATIONS = 40
+
 
 def binatodeci(binary):
     return sum(val * (2**idx) for idx, val in enumerate(reversed(binary)))
@@ -20,26 +22,35 @@ def state_count(r: int):
     return 2**(1 + 2*r)
 
 
-def wolfram_number_to_rule(number: int, r: int):
+def wolfram_code_to_rule(number: int, r: int):
     rule = list(map(int, bin(number)[2:].rjust(state_count(r), '0')))
     return rule
 
 
-def visualize_state(state: np.ndarray, chars: str = ' X'):
+def print_state(state: np.ndarray, chars: str = ' X'):
     print(''.join(map(lambda c: chars[c], state)))
 
 
 if __name__ == '__main__':
-    rule = 90
-    r = 2
+    # handle user inputs
+    r = int(input('range: '))
+    if r > 2 or r < 1:
+        raise Exception('range can only be 1 or 2')
 
-    state = np.zeros(84, dtype=np.int8)
-    state[42] = 1
-    rule = wolfram_number_to_rule(rule, r)
+    wolfram_code = int(input('wolfram code: '))
+    rule = wolfram_code_to_rule(wolfram_code, r)
 
-    print(rule)
+    initialization = input('initialization method ([S]eed/[R]andom): ')
+    if initialization == 'S':
+        state = np.zeros(84, dtype=np.int8)
+        state[42] = 1
+    elif initialization == 'R':
+        state = np.random.randint(0, 2, 84)
+    else:
+        raise Exception('only S and R are valid inputs')
 
-    visualize_state(state)
-    for _ in range(20):
+    # run update loop
+    print_state(state)
+    for _ in range(NUM_ITERATIONS):
         state = update(state, rule, r)
-        visualize_state(state)
+        print_state(state)
