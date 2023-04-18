f04aaaebc6937723e80c22665523f34b0dd10dd8
tobi <Tobias20.3@hotmail.de>
2023-04-18 13:10:04 +0200

    Add support for dynamic range

--- cellular_automaton/main.py
+++ cellular_automaton/main.py
@@ -5,19 +5,23 @@ def binatodeci(binary):
     return sum(val * (2**idx) for idx, val in enumerate(reversed(binary)))
 
 
-def update(state: np.ndarray, rule: list):
+def update(state: np.ndarray, rule: list, r: int):
     new_state = np.zeros_like(state)
 
     for index in range(2, len(state) - 2):
-        input_area = state[index - 1: index + 2]
-        pattern_index = 7 - binatodeci(input_area)  # L=8
+        input_area = state[index - r: index + r + 1]
+        pattern_index = state_count(r) - (binatodeci(input_area)+1)
         new_state[index] = rule[pattern_index]  # apply rule
 
     return new_state
 
 
-def wolfram_number_to_rule(number: int):
-    rule = list(map(int, bin(number)[2:].rjust(8, '0')))   # TODO: assumes l=8
+def state_count(r: int):
+    return 2**(1 + 2*r)
+
+
+def wolfram_number_to_rule(number: int, r: int):
+    rule = list(map(int, bin(number)[2:].rjust(state_count(r), '0')))
     return rule
 
 
@@ -26,11 +30,16 @@ def visualize_state(state: np.ndarray, chars: str = ' X'):
 
 
 if __name__ == '__main__':
+    rule = 90
+    r = 2
+
     state = np.zeros(84, dtype=np.int8)
     state[42] = 1
-    rule = wolfram_number_to_rule(90)
+    rule = wolfram_number_to_rule(rule, r)
+
+    print(rule)
 
     visualize_state(state)
-    for _ in range(40):
-        state = update(state, rule)
+    for _ in range(20):
+        state = update(state, rule, r)
         visualize_state(state)
